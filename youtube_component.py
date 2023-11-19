dcaeb4f670f6c8cbddfa8be14dde8d4cc7ff94ec
hanlun <whl961210@gmail.com>
2023-11-19 01:00:57 -0800

    updated youtube component to get more comments

--- youtube_component.py
+++ youtube_component.py
@@ -11,35 +11,33 @@ def get_youtube_comments(video_id):
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
-    request = youtube.commentThreads().list(
-        part="snippet",
-        videoId=video_id,
-        maxResults=500
-    )
-    response = request.execute()
-
     comments = []
-    for item in response['items']:
-        comment = item['snippet']['topLevelComment']['snippet']
-        text = comment['textDisplay']
+    page_token = None
+    while len(comments) < 300:
+        request = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            maxResults=100,  # Max allowed by YouTube API
+            pageToken=page_token
+        )
+        response = request.execute()
 
-        # Regex to match non-English characters
-        if re.match("^[a-zA-Z0-9\s,.'-?!]*$", text):
-            comments.append([
-                comment['authorDisplayName'],
-                comment['publishedAt'],
-                comment['updatedAt'],
-                comment['likeCount'],
-                text
-            ])
-        '''
-        comments.append([
-            comment['authorDisplayName'],
-            comment['publishedAt'],
-            comment['updatedAt'],
-            comment['likeCount'],
-            comment['textDisplay']
-        ])
-        '''
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']
+            text = comment['textDisplay']
+
+            # Regex to match non-English characters
+            if re.match("^[a-zA-Z0-9\s,.'-?!]*$", text):
+                comments.append([
+                    comment['authorDisplayName'],
+                    comment['publishedAt'],
+                    comment['updatedAt'],
+                    comment['likeCount'],
+                    text
+                ])
+
+        page_token = response.get('nextPageToken')
+        if not page_token:  # Break the loop if there is no next page
+            break
 
     return pd.DataFrame(comments, columns=['author', 'published_at', 'updated_at', 'like_count', 'text'])
