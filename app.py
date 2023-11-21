649185a6406c5f8a7784dfabc9d2e5513b2ddb06
hanlun <whl961210@gmail.com>
2023-11-21 12:56:22 -0800

    modified path to store the db

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from  youtube_component import get_youtube_comments
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////instance/feedback.db'
 db = SQLAlchemy(app)
 CORS(app)
 
